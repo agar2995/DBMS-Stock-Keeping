1588180b1d27001850ef050f29a81c2736e1e938
Aman Agarwal <agarwal.aman2995@gmail.com>
2016-04-07 19:11:32 +0530

    Added files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.div4').hide();
+    $('.div6').hide();
+    
+    $('.user').on('click', function(){
+        $('.div4').toggle(1000);
+        $('.div6').hide(1000);
+        $('.div5').addClass('anim');
+    });
+
+    $('.admin').on('click', function(){
+        $('.div6').toggle(1000);
+        $('.div4').hide(1000);
+    });
+
+    
+});
